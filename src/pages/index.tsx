dd796915de9e0dd2c4599149f797fd9596890a50
Richard Schmidt <richardnew12511@gmail.com>
2023-05-07 11:03:29 -0500

    optimized images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { api, RouterOutputs } from "~/utils/api";
+import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 
 dayjs.extend(relativeTime);
 
@@ -13,10 +15,12 @@ function CreatePostWizard() {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -31,7 +35,13 @@ function PostView(props: PostWithUser) {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full" />
+      <Image
+        src={author.profileImageUrl}
+        className="h-14 w-14 rounded-full"
+        alt={`${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
         <div className="flex gap-2 text-slate-400">
           <span>{`@${author.username}`}</span> ·{" "}
